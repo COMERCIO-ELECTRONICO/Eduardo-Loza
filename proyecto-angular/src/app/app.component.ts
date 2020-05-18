c4cf875738f38cd605ff0b543756d5c1190d6690
n1c0145 <nicolas.lozalml@gmail.com>
2020-05-18 17:02:27 -0500

    Deber pagina responsive

--- proyecto-angular/src/app/app.component.ts
+++ proyecto-angular/src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'proyecto-angular';
+  esconderHola = false;
+  arregloNumeros = [{nombre: 'nicolas'},{nombre: 'eduardo'}];
+  
+  eventoClick() {
+    this.esconderHola = true;
+  }
 }
