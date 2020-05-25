f67c09d3198f52898292829f783f953f893a7d41
n1c0145 <nicolas.lozalml@gmail.com>
2020-05-25 12:53:52 -0500

    deber ruta profesor

--- ejercicio/src/app/app.module.ts
+++ ejercicio/src/app/app.module.ts
@@ -1,28 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutes } from './app.routes';
+import { AppRoutes } from '../app/app.routes';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { IniciarSesionComponent } from './iniciar-sesion/iniciar-sesion.component';
 import { LoginComponent } from './login/login.component';
 import { NoEncontradoComponent } from './no-encontrado/no-encontrado.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {ButtonModule} from 'primeng/button';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { PerfilUsuarioComponent } from './perfil-usuario/perfil-usuario.component';
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     IniciarSesionComponent,
     LoginComponent,
-    NoEncontradoComponent
+    NoEncontradoComponent,
+    PerfilUsuarioComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutes,
     BrowserAnimationsModule,
-    ButtonModule
+    ButtonModule,
+    AppRoutes,
+    FormsModule,
+    MatInputModule,
+    AutoCompleteModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- ejercicio/src/app/login/login.component.ts
+++ ejercicio/src/app/login/login.component.ts
@@ -1,15 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  // credenciales
 
-  constructor() { }
+  correo = '';
+  pass = '';
+  seleccionadoValor;
 
-  ngOnInit(): void {
+  valorAutocomplete = '';
+  arregloResultado = [];
+  sugerencias = ['Estudiante', 'Profesor'];
+
+  constructor(private readonly _router: Router) {}
+
+  ngOnInit(): void {}
+
+  buscarSugerencia(evento) {
+    console.log(evento.query);
+    if (evento.query !== '') {
+      const valorEncontrado = this.sugerencias.find((item) => {
+        return item === evento.query;
+      });
+      console.log(valorEncontrado);
+      this.arregloResultado.push(valorEncontrado);
+      this.sugerencias = this.arregloResultado;
+    } else {
+      this.sugerencias = ['Estudiante', 'Profesor'];
+    }
   }
 
+  valorSeleccionado(evento) {
+    console.log(evento);
+    this.seleccionadoValor = evento;
+  }
+
+  ingresar() {
+    console.log(this.valorAutocomplete);
+
+    if (this.pass === '1234') {
+      alert(this.correo);
+      if (this.seleccionadoValor === 'Estudiante') {
+        alert('es estudiante');
+        this._router.navigate(['/estudiante', 'perfil']);
+      }
+      if (this.seleccionadoValor === 'Profesor') {
+        alert('es profesor');
+        this._router.navigate(['/profesor', 'perfil']);
+      }
+    } else {
+      alert('no ingreso');
+    }
+  }
 }
