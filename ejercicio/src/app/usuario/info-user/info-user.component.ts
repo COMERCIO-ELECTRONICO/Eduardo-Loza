ac49090f9758f9ba119f471f02c69a8f2ecf0edc
n1c0145 <nicolas.lozalml@gmail.com>
2020-05-27 11:46:55 -0500

    Back-end

--- ejercicio/src/app/usuario/info-user/info-user.component.ts
+++ ejercicio/src/app/usuario/info-user/info-user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute} from '@angular/Router';
 @Component({
   selector: 'app-info-user',
   templateUrl: './info-user.component.html',
